c1b3cb95aab735049a1e26e53f17c17d782d06c8
chidera nwokoye <chideranwokoye555@gmail.com>
2024-08-13 12:58:48 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { RouterProvider } from "react-router-dom";
+// import { RouterProvider } from "react-router-dom";
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(
